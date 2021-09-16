1f73763eb2d6d149947de51f15c9223e8684861d
gulersuleyman <ggulersuleyman@gmail.com>
2021-09-16 16:28:30 +0300

    Changes

--- Wheel Roller/Assets/GameFolders/Scripts/Concrete/Controllers/ObstacleController.cs
+++ Wheel Roller/Assets/GameFolders/Scripts/Concrete/Controllers/ObstacleController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class ObstacleController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    float movementFactor;
+    [SerializeField] float period = 2f;
+
     void Start()
     {
-        
+        startingPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (period == 0f) // so the cycles will not be divided by 0
+        {
+            return;
+        }
+
+        float cycles = Time.time / period; // continually growing over time
+
+        const float tau = Mathf.PI * 2; // constant value of 6.283...
+        float rawSinWave = Mathf.Sin(cycles * tau); // Going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1 so it's cleaner
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
     }
 }
