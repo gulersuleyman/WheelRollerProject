f4cbfa78766d4b5af0a24252fafe6a76eda5544c
gulersuleyman <ggulersuleyman@gmail.com>
2021-09-16 21:47:26 +0300

    deneme

--- Wheel Roller/Assets/GameFolders/Scripts/Concrete/Controllers/ObstacleController.cs
+++ Wheel Roller/Assets/GameFolders/Scripts/Concrete/Controllers/ObstacleController.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 
 public class ObstacleController : MonoBehaviour
 {
-    Vector3 startingPosition;
-    [SerializeField] Vector3 movementVector;
-    float movementFactor;
-    [SerializeField] float period = 2f;
+    Vector3 _startingPosition;
+    [SerializeField] Vector3 _movementVector;
+    float _movementFactor;
+    [SerializeField] float _period = 2f;
 
     void Start()
     {
-        startingPosition = transform.position;
+        _startingPosition = transform.position;
     }
 
     void Update()
     {
-        if (period == 0f) // so the cycles will not be divided by 0
+        if (_period == 0f) // so the cycles will not be divided by 0
         {
             return;
         }
 
-        float cycles = Time.time / period; // continually growing over time
+        float cycles = Time.time / _period; // continually growing over time
 
         const float tau = Mathf.PI * 2; // constant value of 6.283...
         float rawSinWave = Mathf.Sin(cycles * tau); // Going from -1 to 1
 
-        movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1 so it's cleaner
+        _movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1 so it's cleaner
 
-        Vector3 offset = movementVector * movementFactor;
-        transform.position = startingPosition + offset;
+        Vector3 offset = _movementVector * _movementFactor;
+        transform.position = _startingPosition + offset;
     }
 }
