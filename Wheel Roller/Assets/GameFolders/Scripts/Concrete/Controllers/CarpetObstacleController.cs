5988e5c1358689696bbbe48925d58b406d35aa08
gulersuleyman <ggulersuleyman@gmail.com>
2021-09-17 17:19:47 +0300

    Obstacles
    Tasarim yapildi

---
+++ Wheel Roller/Assets/GameFolders/Scripts/Concrete/Controllers/CarpetObstacleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarpetObstacleController : MonoBehaviour
+{
+    Vector3 _wheelSize;
+    float _downSizeIndex=8f;
+  
+    private void OnTriggerEnter(Collider collision)
+    {
+        PlayerController player = collision.GetComponent<PlayerController>();
+
+        if (player != null)
+        {
+            WheelSizeAction(player);
+        }
+       
+    }
+    
+    private void WheelSizeAction(PlayerController player)
+    {
+        _wheelSize = player._wheel.transform.localScale;
+        _wheelSize.z -= _downSizeIndex;
+        player._wheel.transform.localScale = _wheelSize;
+    }
+}
